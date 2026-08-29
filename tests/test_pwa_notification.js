c257c0dff6b8f9c0b5a988894407a63b4611b775
tsaibohau <tsaibohau@gmail.com>
2026-08-29 12:43:42 +0800

    Improve semantic announcement search

--- tests/test_pwa_notification.js
+++ tests/test_pwa_notification.js
@@ -671,7 +671,7 @@ function testServiceWorkerContract() {
   assert.match(appSource, /data-read-id/);
   assert.match(appSource, /read\.upsert/);
   assert.match(appSource, /it\.date is publication date/);
-  assert.match(swSource, /cy-news-v41/);
+  assert.match(swSource, /cy-news-v42/);
   assert.match(swSource, /addEventListener\("push"/);
   assert.match(swSource, /showNotification/);
   assert.match(swSource, /addEventListener\("notificationclick"/);
